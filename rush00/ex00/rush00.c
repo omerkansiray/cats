3288e0865f31aa8227201ff4d61980c6fd2b0124
omerkansiray <155628998+omerkansiray@users.noreply.github.com>
2024-07-06 17:27:00 +0300

    Add files via upload

---
+++ rush00/ex00/rush00.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okansira <okansira@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/06 14:25:35 by okansira          #+#    #+#             */
+/*   Updated: 2024/07/06 14:58:22 by okansira         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+void	ft_putchar(char c);
+
+void	ft_print(int len, char begin_char, char midel_char, char end_char)
+{
+	int		letter_counter;
+
+	letter_counter = 1;
+	while (letter_counter <= len)
+	{
+		if (letter_counter == 1)
+			ft_putchar(begin_char);
+		else if (letter_counter == len)
+			ft_putchar (end_char);
+		else
+			ft_putchar(midel_char);
+		letter_counter++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int		line_counter;
+
+	line_counter = 1;
+	if (x >= 1 && y >= 1)
+	{
+		while (line_counter <= y)
+		{
+			if (line_counter == 1)
+				ft_print(x, 'o', '-', 'o');
+			else if (line_counter == y)
+				ft_print (x, 'o', '-', 'o');
+			else
+				ft_print (x, '|', ' ', '|');
+			line_counter++;
+		}
+	}
+}
